6c05a2db657386d0d40cc954989889e1d0df65f7
Sorati <egor.dmitrenko.job@gmail.com>
2020-12-28 16:50:29 +0300

    Created simple class Particle

---
+++ src/classes/particles.js
@@ -0,0 +1,40 @@
+export default class Particles{
+    constructor(canvas = null, maxVelocity = 0.1, particleRadius = 3, particleColor = 'rgba(250, 250, 40, 1)'){
+        this.canvas = canvas
+        this.ctx = canvas.getContext('2d')
+        this.maxVelocity = maxVelocity
+        this.particleRadius = particleRadius
+        this.particleColor = particleColor
+
+        this.x = Math.random()*this.canvas.width;
+        this.y = Math.random()*this.canvas.height;
+
+        this.velocityX = Math.random()*(this.maxVelocity*2) - this.maxVelocity
+        this.velocityY = Math.random()*(this.maxVelocity*2) - this.maxVelocity
+
+        // console.log(canvas.width)
+    }
+
+    position(){
+        if ((this.x > this.canvas.width || this.x < 0)  || (this.y > this.canvas.height || this.y < 0 )){
+            this.x = Math.random()*this.canvas.width
+            this.velocityX = Math.random()*(properties.particleMaxVelocity*2) - properties.particleMaxVelocity
+
+            this.y = Math.random()*this.canvas.height
+            this.velocityY = Math.random()*(properties.particleMaxVelocity*2) - properties.particleMaxVelocity
+        }
+
+        this.x += this.velocityX
+        this.y += this.velocityY
+
+        // console.log(`X:${this.x} Y:${this.y}`)
+    }
+
+    reDrow(){
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, this.particleRadius, 0, Math.PI*2);
+        this.ctx.closePath();
+        this.ctx.fillStyle = this.particleColor;
+        this.ctx.fill();
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,4 +1,58 @@
 import "./styles/main.scss";
 import PoolManager from './classes/pool-manager'
+import Particles from './classes/particles'
+
+
+let canvas = document.createElement('canvas'),
+    ctx = canvas.getContext('2d'),
+    // particles = [],
+    properties = {
+        bgColor: 'rgba(0, 0, 0, 0)',
+        particleColor: 'rgba(250, 250, 40, 1)',
+        particleRadius: 13,
+        particleCount: 100,
+        particleMaxVelocity: 0.03,
+        // lifetime: 5000
+    }
+
+canvas.width = innerWidth
+canvas.height = innerHeight
+
+document.querySelector('body').appendChild(canvas);
 
 const poolManager = new PoolManager()
+const star = new Particles(canvas, properties.particleMaxVelocity, properties.particleRadius)
+
+window.onresize = function () {
+    canvas.width = innerWidth;
+    canvas.height = innerHeight;
+    // console.log(canvas)
+}
+
+/**************************************************/
+
+function reDrowBackground() {
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle = properties.bgColor;
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+}
+
+// function reDrowParticles(){
+//     for(let i in particles){
+//         particles[i].position();
+//         particles[i].reDrow();
+//     }
+// }
+
+// function loop(){
+//     reDrowBackground()
+//     reDrowParticles();
+//     requestAnimationFrame(loop);
+// }
+
+// function init(){
+//     for(let i = 0; i < properties.particleCount; i++){
+//         particles.push(new Particle);
+//     }
+//     loop();
+// }
